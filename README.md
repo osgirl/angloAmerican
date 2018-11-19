c9e3c7cd8d0f49ebd5a6fce140ad95de04f68204
Vijaya Jiwalag Tiple <vijaya.tiple@yash.com>
2018-11-19 12:08:56 +0530

    Renamed to anglo american

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-angular First application
+anglo american application
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.6.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular First application'`, () => {
+  it(`should have as title 'anglo american application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular First application');
+    expect(app.title).toEqual('anglo american application');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('angular First application!');
+    expect(compiled.querySelector('h1').textContent).toContain('anglo american application!');
   });
 });
